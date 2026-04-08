97682ee471b6db96170dba829f6ee8e34f24c674
Artem <ag2650920@gmail.com>
2026-04-08 15:22:47 +0300

    Task10

--- Task10Project/logic.cpp
+++ Task10Project/logic.cpp
@@ -3,3 +3,17 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая подсчитывает количество положительных элементов вектора.
+
+#include "logic.h"
+
+int count_of_positive_elements(int vector[], int size) {
+	if (size == 0) {
+		return 0;
+	}
+	int remainingCount = count_of_positive_elements(vector, size - 1);
+
+	if (vector[size - 1] > 0) {
+		return 1 + remainingCount;
+	}
+	return remainingCount;
+}
