4074949c88bdc276368f18f6422ff90cad7b4942
Artem <ag2650920@gmail.com>
2026-04-08 15:28:46 +0300

    Task11

--- Task11Project/logic.cpp
+++ Task11Project/logic.cpp
@@ -3,3 +3,18 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет среднее арифметическое элементов вектора.
+#include "logic.h"
+
+double sum_elements(int vector[], int size) {
+	if (size == 0) {
+		return 0;
+	}
+	return vector[size - 1] + sum_elements(vector, size - 1);
+}
+double calculate_average(int vector[], int size) {
+	if (size == 0) {
+		return 0.0;
+	}
+	int sum = sum_elements(vector, size);
+	return(double)sum / size;
+}
