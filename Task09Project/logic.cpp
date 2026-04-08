a1012546139e60d97340ab8b40a9ee5336fd6c96
Artem <ag2650920@gmail.com>
2026-04-08 15:18:30 +0300

    Task09

--- Task09Project/logic.cpp
+++ Task09Project/logic.cpp
@@ -3,3 +3,20 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму цифр заданного числа, кратных 5
+
+#include "logic.h"
+
+int sum(int number) {
+	if (number == 0) {
+		return 0;
+	}
+
+	int lastDigit = number % 10;
+
+	int remainingSum = sum(number / 10);
+
+	if (lastDigit % 5 == 0) {
+		return lastDigit + remainingSum;
+	}
+	return remainingSum;
+}
